7a0ec6e1f4231bbe94fc5ca1ea73dccf33cace15
gparedes0610 <paredesgustavo.0610@gmail.com>
2021-11-11 12:05:39 -0500

    React creado

---
+++ proyectproductfef/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+//import { bootstrap } from "bootstrap/dist/css/bootstrap.min.css";
+function App() {
+  return (
+    <>
+      <h2>Hola</h2>
+      <button className="btn btn-primary">holaa</button>
+    </>
+  );
+}
+
+export default App;
